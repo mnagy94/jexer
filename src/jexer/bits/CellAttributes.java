fe69ce063b68f92d4990c072c8872f4732c3d32e
Kevin Lamonte <kevin.lamonte@gmail.com>
2018-03-25 14:30:22 -0400

    fix javadoc

--- src/jexer/bits/CellAttributes.java
+++ src/jexer/bits/CellAttributes.java
@@ -248,7 +248,7 @@ public class CellAttributes {
     /**
      * Setter for foreColor RGB.
      *
-     * @param foreColor new foreColor RGB value
+     * @param foreColorRGB new foreColor RGB value
      */
     public final void setForeColorRGB(final int foreColorRGB) {
         this.foreColorRGB = foreColorRGB;
@@ -266,7 +266,7 @@ public class CellAttributes {
     /**
      * Setter for backColor RGB.
      *
-     * @param backColor new backColor RGB value
+     * @param backColorRGB new backColor RGB value
      */
     public final void setBackColorRGB(final int backColorRGB) {
         this.backColorRGB = backColorRGB;
