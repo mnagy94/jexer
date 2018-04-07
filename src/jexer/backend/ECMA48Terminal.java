5f3f6a54cd128192241ed21a35984aaf1b100c10
Kevin Lamonte <kevin.lamonte@gmail.com>
2018-04-07 13:49:32 -0400

    #29 avoid potential NPE

--- src/jexer/backend/ECMA48Terminal.java
+++ src/jexer/backend/ECMA48Terminal.java
@@ -507,9 +507,12 @@ public class ECMA48Terminal extends LogicalScreen
             e.printStackTrace();
         }
 
-        // Disable mouse reporting and show cursor
-        output.printf("%s%s%s", mouse(false), cursor(true), normal());
-        output.flush();
+        // Disable mouse reporting and show cursor.  Defensive null check
+        // here in case closeTerminal() is called twice.
+        if (output != null) {
+            output.printf("%s%s%s", mouse(false), cursor(true), normal());
+            output.flush();
+        }
 
         if (setRawMode) {
             sttyCooked();
