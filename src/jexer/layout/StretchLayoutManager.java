0e9957018d5d796258cd89f336616875ea35cdf7
Kevin Lamonte <kevin.lamonte@gmail.com>
2019-08-17 07:44:53 -0500

    #46 stubs for LayoutManager

---
+++ src/jexer/layout/StretchLayoutManager.java
@@ -0,0 +1,81 @@
+/*
+ * Jexer - Java Text User Interface
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2019 Kevin Lamonte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * @author Kevin Lamonte [kevin.lamonte@gmail.com]
+ * @version 1
+ */
+package jexer.layout;
+
+import jexer.TWidget;
+import jexer.event.TResizeEvent;
+
+/**
+ * StretchLayoutManager repositions child widgets based on their coordinates
+ * when added and the current widget size.
+ */
+public class StretchLayoutManager implements LayoutManager {
+
+    // ------------------------------------------------------------------------
+    // Variables --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+
+    // ------------------------------------------------------------------------
+    // Constructors -----------------------------------------------------------
+    // ------------------------------------------------------------------------
+
+
+    // ------------------------------------------------------------------------
+    // LayoutManager ----------------------------------------------------------
+    // ------------------------------------------------------------------------
+
+    /**
+     * Process the parent widget's resize event, and resize/reposition child
+     * widgets.
+     *
+     * @param resize resize event
+     */
+    public void onResize(final TResizeEvent resize) {
+        // TODO
+    }
+
+    /**
+     * Add a child widget to manage.
+     *
+     * @param child the widget to manage
+     */
+    public void add(final TWidget child) {
+        // TODO
+    }
+
+    /**
+     * Remove a child widget from those managed by this LayoutManager.
+     *
+     * @param child the widget to remove
+     */
+    public void remove(final TWidget child) {
+        // TODO
+    }
+
+}
